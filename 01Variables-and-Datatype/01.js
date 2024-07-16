09404f650c2bbb24b59ff871d9113e7c29c2e2ed
Pranav Ahuja <72250259+kingpranav21@users.noreply.github.com>
2024-07-16 14:35:56 +0530

    Day-01

---
+++ 01Variables-and-Datatype/01.js
@@ -0,0 +1,42 @@
+//Task - 1
+var num = 8;
+console.log(num);
+
+//Task - 2
+let str = "Pranav";
+console.log(str);
+
+//Task - 3
+const isHappy = false;
+console.log(isHappy);
+
+//Task - 4
+const hero = {
+  lvl: 4,
+  usp: "attack",
+  special_power: "JetPack",
+};
+console.log(typeof hero);
+console.log(typeof num);
+console.log(typeof str);
+console.log(typeof isHappy);
+
+const arr = [1, 2, 3, 4];
+
+const new_arr = [...arr, 5];
+
+console.log(typeof arr);
+console.log(new_arr[4]);
+console.log(new_arr[3]);
+
+//Task - 5
+let a = 54;
+console.log(a);
+a = "makeit";
+console.log(a);
+a = 3;
+console.log(a);
+
+//Task - 6
+// isHappy = true;
+// console.log(isHappy); //Error : "Assignment to a constant variable"
