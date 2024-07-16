258ce0b8fb2d848f1def426747b27f7ac9baeed2
Pranav Ahuja <72250259+kingpranav21@users.noreply.github.com>
2024-07-16 16:45:38 +0530

    Day-02

---
+++ 02Operators/Operator.js
@@ -0,0 +1,99 @@
+//Activity - 1
+
+//task -1
+let a = 5;
+let b = 3;
+let sum = a + b;
+console.log(sum);
+
+//task - 2
+let diff = a - b;
+console.log(diff);
+
+//task - 3
+let prod = a * b;
+console.log(prod);
+
+//task - 4
+b = 10;
+let div = b / a;
+console.log(div);
+
+//task - 5
+b = 7;
+let rem = b % a;
+console.log(rem);
+
+//Activity - 2
+
+//task - 6
+let count = 0;
+count += 8;
+count += 5;
+
+console.log(count);
+
+//task - 7
+count -= 12;
+
+console.log(count);
+
+//Activity - 3
+
+// task - 8
+let a_ = 4;
+let b_ = -2;
+
+if (a_ < b_) {
+  console.log("a_ is smaller");
+} else {
+  console.log("b_ is smaller");
+}
+
+b_ = 5;
+a_ > b_ ? console.log("a_ is greater") : console.log("b_ is greater");
+
+// task - 9
+a_ = 6;
+a_ >= b_
+  ? console.log("a_ is greater than/equal to b_")
+  : console.log("b_ is greater than / equal to a_");
+
+b_ = 10;
+a_ >= b_
+  ? console.log("a_ is greater than/equal to b_")
+  : console.log("b_ is greater than / equal to a_");
+
+//task - 10
+
+a_ = "10";
+
+a_ == b_ ? console.log("true") : console.log("false");
+
+a_ === b_ ? console.log("true") : console.log("false"); //it compares both values and datatypes
+
+//Activity - 4
+
+//task - 11
+
+let Players = 11;
+let Bats = 4;
+
+Players > 9 && Bats > 2 ? console.log("Can Play") : console.log("Can't Play");
+
+//task - 12
+Bats = 0;
+Players = 1;
+Players > 9 || Bats > 2 ? console.log("Can Play") : console.log("Can't Play");
+
+//task - 13
+let canPlay = false;
+
+console.log(!canPlay);
+
+
+//Activity - 5
+
+//task - 14
+let num = 12;
+num > 0 ? console.log("+ve") : console.log("-ve");
