299f63d1e16273921f580cad82bbc3244fea23c9
Pranav Ahuja <72250259+kingpranav21@users.noreply.github.com>
2024-07-24 23:39:23 +0530

    Day-12

--- 12Error-Handling/Error.js
+++ 12Error-Handling/Error.js
@@ -1,6 +1,6 @@
 //Activity - 1 Basic Error Handling with Try-Catch
 
-const { useSyncExternalStore } = require("react");
+
 
 //task - 1 Write a function that intentionally throws an error and use a try-catch block to handle the error and log an appropriate message to the console.
 
